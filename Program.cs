fd91ab4fc0f4f64b8222982741361d4370b74db2
kirill <awerin134@gmail.com>
2022-11-22 20:35:09 +0300

    secondе

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите x1");
+double x = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите y1");
+double y = double.Parse(Console.ReadLine());
+double sum = 0;
+System.Console.WriteLine("Введите x2");
+double x2 = double.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите y2");
+double y2 = double.Parse(Console.ReadLine());
+
+sum = Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
+System.Console.WriteLine("Расстояние между 2-мя точками =" + sum);
