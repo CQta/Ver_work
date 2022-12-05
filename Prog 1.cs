97826104c57be1257ca23e0c50ed378bcdbd2dff
CQta <awerin134@gmail.com>
2022-12-05 18:05:08 +0300

    2

---
+++ Prog 1.cs
@@ -0,0 +1,26 @@
+﻿int[] GetArray(int m) 
+{
+    int[] result = new int[m];
+
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write("Введите цисло: ");
+        result[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+         Console.Write($"{inArray[i]}\t ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int rows = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(rows);
+PrintArray(array);
